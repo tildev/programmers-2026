d3f7dafd890f67fc31f6a507092f58d79a4e43ea
tildev <tildevelove@gmail.com>
2026-04-03 00:47:10 +0900

    feat: Add solution

---
+++ src/main/java/programmers/l340207/Solution.java
@@ -0,0 +1,10 @@
+package programmers.l340207;
+
+public class Solution {
+    public static void main(String[] arg){
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
