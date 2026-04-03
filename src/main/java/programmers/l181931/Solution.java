4d0d356a1032f56281d1780ea2c30d84fabb3b5b
tildev <tildevelove@gmail.com>
2026-04-04 00:35:38 +0900

    feat: Add solution

--- src/main/java/programmers/l181931/Solution.java
+++ src/main/java/programmers/l181931/Solution.java
@@ -1,5 +1,7 @@
 package programmers.l181931;
 
+import java.util.stream.IntStream;
+
 class Solution {
   public int solution(int a, int d, boolean[] included) {
     int answer = 0;
@@ -11,6 +13,13 @@ class Solution {
       a += d;
     }
     return answer;
+  }
+
+  public int solution1(int a, int d, boolean[] included) {
+    return IntStream.range(0, included.length)
+            .filter(i -> included[i])
+            .map(i -> a + (i * d))
+            .sum();
   }
 
   public static void main(String[] args) {
